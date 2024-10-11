d4b31e771802c652ba94378924961d67eca7db18
BALASAITEJA <balasaiteja@BALASAITEJAs-MacBook-Air.local>
2024-10-11 16:42:53 +0530

    wraped whole js code into DOMContentLoaded event listener

--- ToDo-List/script.js
+++ ToDo-List/script.js
@@ -1,25 +1,27 @@
-let input = document.getElementById("todo-input");
-let addTask = document.getElementById("add-task-btn");
-let TasksList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  let input = document.getElementById("todo-input");
+  let addTask = document.getElementById("add-task-btn");
+  let TasksList = document.getElementById("todo-list");
 
-let Tasks = [];
+  let Tasks = [];
 
-// after clicking on add task button this happens
-addTask.addEventListener("click", () => {
-  const taskText = input.value.trim();
-  if (taskText === "false") return;
+  // after clicking on add task button this happens
+  addTask.addEventListener("click", () => {
+    const taskText = input.value.trim();
+    if (taskText === "false") return;
 
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    completed: false,
-  };
-  Tasks.push(newTask); // add the input into the array Tasks
-  saveTasks(); // we are saving the tasks in localStorage
-  input.value = ""; // clearing the input once it is saved
-  // console.log(Tasks);
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    Tasks.push(newTask); // add the input into the array Tasks
+    saveTasks(); // we are saving the tasks in localStorage
+    input.value = ""; // clearing the input once it is saved
+    // console.log(Tasks);
+  });
+
+  function saveTasks() {
+    localStorage.setItem("Tasks", JSON.stringify(Tasks));
+  }
 });
-
-function saveTasks() {
-  localStorage.setItem("Tasks", JSON.stringify(Tasks));
-}
